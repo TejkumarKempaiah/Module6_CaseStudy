5f3f5667b8b684d3f5185929019ef51958245f7c
Tejkumar <>
2020-03-11 19:28:50 +0530

    Added java file

---
+++ src/medicalProject/ShipmentService.java
@@ -0,0 +1,23 @@
+package medicalProject;
+
+public class ShipmentService 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("----Printing the Shipment Servie Details----");System.out.println("");
+		System.out.println("Shipment ID = 100");
+		System.out.println("Order ID = 101");
+		System.out.println("Resource ID = 1234");
+		System.out.println("Shipment location = Bangalore");
+		System.out.println("Firstname = Tejkumar");
+		System.out.println("Lastname = Kempaiah");
+		System.out.println("Postal Address = Dwaraka Apartment, Bangalore");
+		System.out.println("Shipment Date: 11th March 2020");
+		System.out.println("Expired Date: 11th March 2025");
+		System.out.println("Delivery Text - Edureka Delivery");
+		System.out.println("Delivery Date: 13th March 2020");
+		System.out.println("Tracking number = Invoice12345");
+		System.out.println("Charged Amount = 200USD");
+	}
+}
